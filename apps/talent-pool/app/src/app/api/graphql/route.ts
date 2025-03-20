91e5fcb5c819e7b5e2babff8784c6edcb7b8bff5
Austin Biggs <austintbiggs@gmail.com>
2025-03-20 13:34:57 -0500

    refactor: move talent-pool into /app subdirectory

--- apps/talent-pool/app/src/app/api/graphql/route.ts
+++ apps/talent-pool/app/src/app/api/graphql/route.ts
@@ -1,6 +1,6 @@
 import { startServerAndCreateNextHandler } from '@as-integrations/next';
 import { NextRequest } from 'next/server';
-import { server } from 'gql';
+import { server } from 'libs/gql/src';
 
 const handler = startServerAndCreateNextHandler<NextRequest>(server, {
   context: async (req) => ({ req }),
