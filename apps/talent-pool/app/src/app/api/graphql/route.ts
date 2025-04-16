aa7bd82c67f1fd64940522f171e2f642c5668802
Austin Biggs <austintbiggs@gmail.com>
2025-04-15 19:27:24 -0500

    feat: install Tailwind

--- apps/talent-pool/app/src/app/api/graphql/route.ts
+++
@@ -1,9 +0,0 @@
-import { startServerAndCreateNextHandler } from '@as-integrations/next';
-import { NextRequest } from 'next/server';
-import { server } from '@talent/gql';
-
-const handler = startServerAndCreateNextHandler<NextRequest>(server, {
-  context: async (req) => ({ req }),
-});
-
-export { handler as GET, handler as POST };
