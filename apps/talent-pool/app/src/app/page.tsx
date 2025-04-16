d8ec36d57484c58707e7a7c9343d41587aaa7331
Austin Biggs <930864+austinbiggs@users.noreply.github.com>
2025-04-15 19:30:53 -0500

    build: Install Tailwind (#90)
    * build(deps): install Tailwind
    
    * feat: install Tailwind

--- apps/talent-pool/app/src/app/page.tsx
+++ apps/talent-pool/app/src/app/page.tsx
@@ -3,7 +3,7 @@ import styles from './page.module.scss';
 export default function Index() {
   return (
     <div className={styles.page}>
-      Hello World
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </div>
   );
 }
